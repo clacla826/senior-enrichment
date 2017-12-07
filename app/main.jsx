6c5364ce3b2294d3fdf62b6ea85f03ec32836d8f
Claudia Baik <claudiabaik@gmail.com>
2017-12-07 14:49:57 -0500

    initial react setup

--- app/main.jsx
+++ app/main.jsx
@@ -1,14 +1,31 @@
+// 'use strict'
+// import React from 'react'
+// import {render} from 'react-dom'
+// import { Provider } from 'react-redux'
+
+// import store from './store'
+// import Root from './components/Root'
+
+// render (
+//   <Provider store={store}>
+//     <Root/>
+//   </Provider>,
+//   document.getElementById('main')
+// )
+
+
+
+//REACT VERSION//
 'use strict'
 import React from 'react'
-import {render} from 'react-dom'
-import { Provider } from 'react-redux'
+import ReactDOM, {render} from 'react-dom'
 
-import store from './store'
+
 import Root from './components/Root'
 
 render (
-  <Provider store={store}>
-    <Root/>
-  </Provider>,
+  //<Provider store={store}>
+    <Root/>,
+  //</Provider>,
   document.getElementById('main')
 )
