4ae0c00c74712efe8344eaf6cb1f8d54e913df7c
Claudia Baik <claudiabaik@gmail.com>
2017-12-09 09:58:48 -0500

    lifted the state in Root and refetching in AllCampus.jsx

--- app/components/AllCampus.jsx
+++ app/components/AllCampus.jsx
@@ -2,38 +2,132 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+// const AllCampus = (props) => {
+//   const campuses = props.campuses;
+//   console.log("ALLCAMPUS PROPS", props);
+
+//   return (
+//     <div>
+//       <div>
+//         <Link to="/campusform">CREATE A CAMPUS</Link>
+//       </div>
+//       <div className="row">
+//         <h2>OUR CAMPUSES</h2>
+//         {campuses && campuses.map(campus => (
+//           <div className="col-xs-4" key={campus.id}>
+//             <Link className="thumbnail" to={`/campuses/${campus.id}`}>
+//               <img src={campus.imageUrl} />
+//             </Link>
+
+//             <div className="caption">
+//               <h5>
+//                 <span>{campus.name}</span>
+//               </h5>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllCampus;
+
 //bring all campuses but need to be dummy
+// class AllCampus extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       campuses: []
+//     };
+
+//   }
+
+//   componentDidMount() {
+//     axios
+//       .get("/api/campuses")
+//       .then(res => res.data)
+//       .then(campuses => this.setState({ campuses }));
+//   }
+
+//   render() {
+//     const campuses = this.state.campuses;
+//     console.log("ALLCAMPUS THIS STATE", this.state)
+
+//     return (
+//       <div>
+//       <div>
+//       <Link to='/campusform'>CREATE A CAMPUS</Link>
+//       </div>
+//         <div className="row">
+//           <h2>OUR CAMPUSES</h2>
+//           {campuses.map(campus => (
+//             <div className="col-xs-4" key={campus.id}>
+//               <Link  className="thumbnail" to={`/campuses/${campus.id}`}>
+//                 <img src={campus.imageUrl} />
+//               </Link>
+
+//               <div className="caption">
+//                 <h5>
+//                   <span>{campus.name}</span>
+//                 </h5>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default AllCampus;
+
+// componentWillReceiveProps(nextProps) {
+//   console.log("newProps", nextProps); //oldProps
+//   //const currentProps = this.props;
+// //     const nextPlaylistId = nextProps.match.params.playlistId; -Renders if not same const currentPlaylistId = currentProps.match.params.playlistId;
+
+// //     if (nextPlaylistId !== currentPlaylistId) {
+// //  this.fetchPlaylistById(nextPlaylistId);
+// }
+
 class AllCampus extends Component {
   constructor() {
     super();
     this.state = {
       campuses: []
     };
-
   }
 
-  componentDidMount() {
+  fetchCampuses() {
     axios
       .get("/api/campuses")
       .then(res => res.data)
-      .then(campuses => this.setState({ campuses }));
+      .then(campuses => {
+        console.log("fetchCampuses AllCampus axios request", campuses);
+        this.setState({ campuses });
+      });
   }
 
+  componentDidMount() {
+    console.log("didMount", this.props);
+    this.fetchCampuses();
+  }
 
   render() {
     const campuses = this.state.campuses;
-    console.log("ALLCAMPUS THIS STATE", this.state)
+    console.log("ALLCAMPUS THIS STATE", this.state);
 
     return (
       <div>
-      <div>
-      <Link to='/campusform'>CREATE A CAMPUS</Link>
-      </div>
+        <div>
+          <Link to="/campusform">CREATE A CAMPUS</Link>
+        </div>
         <div className="row">
           <h2>OUR CAMPUSES</h2>
           {campuses.map(campus => (
             <div className="col-xs-4" key={campus.id}>
-              <Link  className="thumbnail" to={`/campuses/${campus.id}`}>
+              <Link className="thumbnail" to={`/campuses/${campus.id}`}>
                 <img src={campus.imageUrl} />
               </Link>
 
--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { HashRouter as Router, Route} from 'react-router-dom';
-//import axios from "axios";
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
 
 import Header from "./Header";
 import AllCampus from "./AllCampus";
@@ -9,49 +9,72 @@ import SingleCampus from "./SingleCampus";
 import SingleStudent from "./SingleStudent";
 import EditCampusForm from "./EditCampusForm";
 
+export default class Root extends Component {
+  constructor() {
+    super();
+    this.state = {
+      campuses: []
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get("/api/campuses")
+      .then(res => res.data)
+      .then(campuses => {
+        console.log("hahahah", campuses);
+        this.setState({ campuses });
+      });
+  }
 
-// class Root extends Component {
+  render() {
+    console.log("THIIIS.campuses", this.state.campuses);
 
-//   constructor() {
-//     super();
-//     this.state = {
-//       campuses: []
-//     };
-
-//   }
-
-//   componentDidMount() {
-//     axios
-//       .get("/api/campuses")
-//       .then(res => res.data)
-//       .then(campuses => console.log('hahahah', campuses));
-//   }
-
-
-//   render () {
-//     console.log('THIIIS.campuses', this.state)
-
-const Root = () => {
-return (
+    return (
       <Router>
-      <div id="root" className="container-fluid">
-        <div className="col-xs-12">
-          <Header />
+        <div id="root" className="container-fluid">
+          <div className="col-xs-12">
+            <Header />
+          </div>
+          <div className="col-xs-12">
+            <Route
+              exact
+              path="/"
+              component={AllCampus}
+              campuses={this.state.campuses}
+            />
+            <Route exact path="/campuses" component={AllCampus} />
+            <Route exact path="/campuses/:campusId" component={SingleCampus} />
+            <Route exact path="/students" component={AllStudent} />
+            <Route path="/students/:studentId" component={SingleStudent} />
+            <Route exact path="/campusform" component={EditCampusForm} />
+            <Route path="/campusform/:campusId" component={EditCampusForm} />
+          </div>
         </div>
-        <div className="col-xs-12">
-          <Route exact path='/' component={AllCampus} />
-          <Route exact path='/campuses' component={AllCampus} />
-          <Route exact path='/campuses/:campusId' component={SingleCampus} />
-          <Route exact path='/students' component={AllStudent} />
-          <Route path='/students/:studentId' component={SingleStudent} />
-          <Route exact path='/campusform' component={EditCampusForm} />
-          <Route path='/campusform/:campusId' component={EditCampusForm} />
-        </div>
-      </div>
       </Router>
     );
+  }
 }
 
+// const Root = () => {
+// return (
+//       <Router>
+//       <div id="root" className="container-fluid">
+//         <div className="col-xs-12">
+//           <Header />
+//         </div>
+//         <div className="col-xs-12">
+//           <Route exact path='/' component={AllCampus} />
+//           <Route exact path='/campuses' component={AllCampus} />
+//           <Route exact path='/campuses/:campusId' component={SingleCampus} />
+//           <Route exact path='/students' component={AllStudent} />
+//           <Route path='/students/:studentId' component={SingleStudent} />
+//           <Route exact path='/campusform' component={EditCampusForm} />
+//           <Route path='/campusform/:campusId' component={EditCampusForm} />
+//         </div>
+//       </div>
+//       </Router>
+//     );
+// }
 
-export default Root;
+// export default Root;
